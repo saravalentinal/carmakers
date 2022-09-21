5296982218a51ebae856ece2ea89c425f7a6c1f8
saravalentinal <latynsara41@gmail.com>
2022-09-21 11:20:55 +0200

    ports

--- src/app.js
+++ src/app.js
@@ -59,7 +59,7 @@ app.use('/api', APIproductRoutes);
 })*/
 // Para Heroku
 
-let puerto = 4000
+let puerto = 4040
 app.listen(process.env.PORT||puerto, function(){
     console.log('Servidor funcionando '+ puerto);
 });
